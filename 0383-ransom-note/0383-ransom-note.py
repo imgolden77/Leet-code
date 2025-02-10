b268bb74481c4a89216c5b2fbe0367059d5c17e5
imgolden77 <51187104+imgolden77@users.noreply.github.com>
2025-02-10 21:37:22 +0000

    Time: 27 ms (28.17%), Space: 17.9 MB (76.72%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,10 +1,19 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        ransomNote_list=list(ransomNote)
-        magazine_list=list(magazine)
-        for i in ransomNote_list:
-            if i in magazine_list:
-                magazine_list.remove(i)
+        dictionary = {}
+
+        # Iterate through the magazine and count characters
+        for char in magazine:
+            if char not in dictionary:
+                dictionary[char] = 1
+            else:
+                dictionary[char] += 1
+
+        # Iterate through the ransom note and check character counts
+        for char in ransomNote:
+            if char in dictionary and dictionary[char] > 0:
+                dictionary[char] -= 1
             else:
                 return False
-        return True   
+        
+        return True  
