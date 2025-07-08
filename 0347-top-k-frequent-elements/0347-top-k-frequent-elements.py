d399db1fec7fd78f5eb0b4586bed3baec3604ceb
Juha Im <51187104+imgolden77@users.noreply.github.com>
2025-07-08 17:35:43 +0100

    Time: 7 ms (52.24%), Space: 21.2 MB (61.51%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,18 +1,6 @@
 class Solution:
     def topKFrequent(self, nums, k):
-        count = defaultdict(int)
-        for num in nums:
-            count[num] += 1
+        count = Counter(nums)  # O(n)
 
-        # 버킷: index = 등장 횟수, 값 = 숫자 목록
-        bucket = [[] for _ in range(len(nums)+1)]
-        for num, freq in count.items():
-            bucket[freq].append(num)
-
-        result = []
-        # 등장 횟수가 높은 것부터 거꾸로 순회
-        for i in range(len(bucket)-1, 0, -1):
-            for num in bucket[i]:
-                result.append(num)
-                if len(result) == k:
-                    return result
+        # 가장 흔한 k개 추출 (최소 힙 사용)
+        return [item for item, freq in heapq.nlargest(k, count.items(), key=lambda x: x[1])]
