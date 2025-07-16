9df137ab2186d8a1cddca573c1a79ea4856e0c50
Juha Im <51187104+imgolden77@users.noreply.github.com>
2025-07-16 23:01:39 +0100

    Time: 7 ms (81.05%), Space: 18.4 MB (41.33%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -2,12 +2,9 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         s= ''.join([c for c in s if c.isalpha() or c.isdigit()])
         s= s.lower()
-        # print(s)
-        s_list=list(s)
+        if len(s)== 0 or len(s)== 1:
+            return True
         for i in range(len(s)):
-            if len(s_list)== 0 or len(s_list)== 1:
-                return True
-            elif s_list[i] != s_list[-i-1]:
-                # print(i, s_list[i], s_list[-i-1])
+            if s[i] != s[-i-1]:
                 return False
         return True
