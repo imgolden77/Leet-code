c3ca3395cc73cb724b920c021939b35af888c235
Juha Im <51187104+imgolden77@users.noreply.github.com>
2025-08-14 14:08:55 +0100

    Time: 7 ms (72.58%), Space: 20.9 MB (89.11%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -2,17 +2,11 @@ class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         i=1
         res=[]
-        n= len(intervals)
         intervals.sort()
+        n= len(intervals)
         new=intervals[0]
-        print("debug0:", intervals)
         if n==1:
             return intervals
-
-        while i<n and intervals[i][1]<new[0]:
-            res.append(intervals[i])
-            print("debug1:",i, res)
-            i +=1
             
         while i<n:
             if intervals[i][0]<=new[1]:
@@ -25,7 +19,6 @@ class Solution:
             i+=1  
         
         res.append(new)
-        print("debug3:", i, res)
             
         return res
         
