2c7fcedb5bd57a253450623d04bfa0a2e4352e71
Juha Im <51187104+imgolden77@users.noreply.github.com>
2025-10-02 16:13:48 +0100

    Time: 227 ms (93.41%), Space: 20.2 MB (68.36%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,23 +1,27 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        rows, columns = len(grid), len(grid[0])
+        rows, cols = len(grid), len(grid[0])
         count = 0
-        def dfs(r, c):
-            if r<0 or r>=rows or c<0 or c>=columns or grid[r][c] =='0':
+        queue= deque()
+
+        def bfs(r,c):
+            if r <0 or r>=rows or c<0 or c>=cols or grid[r][c] == '0':
                 return
-            
-            grid[r][c] = '0'
-            
-            dfs(r-1, c)
-            dfs(r+1, c)
-            dfs(r, c-1)
-            dfs(r, c+1)
+            grid[r][c]='0'
+            queue.append((r,c))           
 
         for r in range(rows):
-            for c in range(columns):
+            for c in range(cols):
                 if grid[r][c] == '1':
-                    dfs(r,c)
-                    count += 1
+                    bfs(r,c)
+
+                    while queue:
+                        k, l = queue.popleft()
+                        bfs(k-1, l)
+                        bfs(k+1, l)
+                        bfs(k, l-1)
+                        bfs(k, l+1)
+                    count+=1 
         
         return count
                 
