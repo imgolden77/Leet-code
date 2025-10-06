59ff3732e7ea0b341829e47ff31feeb134a85209
Juha Im <51187104+imgolden77@users.noreply.github.com>
2025-10-06 16:55:36 +0100

    Time: 0 ms (100%), Space: 17.9 MB (65.77%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -2,14 +2,14 @@ class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         n = len(nums)
         res= [[]]
-        def dp(idx):
-            for r in res:
-                curr = r.copy()
-                if nums[idx] not in curr:
-                    curr.append(nums[idx])
-                    res.append(curr)
-                             
+    
         for idx in range(n):
-            dp(idx)
+            subsets=[]
+            for subset in res:
+                curr = subset.copy()
+                curr.append(nums[idx])
+                subsets.append(curr)
+            
+            res.extend(subsets)
         
         return res
