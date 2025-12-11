8ab53f093a02f7461980c43f5943c050f2a6628f
imgolden77 <51187104+imgolden77@users.noreply.github.com>
2025-12-11 20:03:45 +0000

    Time: 0 ms (100%), Space: 18.1 MB (15.27%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,36 +1,18 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
-        former= ['(', '{', '[']
+        par_map = {')':'(', '}':'{', ']':'['}
         
         for i in s:
-            if i in former:
+            if i in par_map.values():
                 stack.append(i)
-            elif i == ')':
-                if stack== []:
-                    return False
-                elif stack[-1]== '(':
-                    stack.pop()
-                else:
-                    return False
-            elif i == '}':
-                if stack== []:
-                    return False
-                elif stack[-1]== '{':
-                    stack.pop()
-                else:
-                    return False
-            elif i == ']':
-                print(stack)
-                if stack == []:
-                    return False
-                elif stack[-1]== '[':
-                    stack.pop()
-                else:
-                    return False
-            
+            elif stack == []:
+                return False
+            elif stack[-1]== par_map[i]:
+                stack.pop()
+            else:
+                return False
         
-        if stack == []:
-            return True
-        else:
-            return False
+        return not stack
+                
+          
